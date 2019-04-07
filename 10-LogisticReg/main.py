4617355f5f9c565369f781eecbca5ee6928d0962
Santiago <santiagomartinezc96@gmail.com>
2019-04-07 17:20:48 -0500

    main.py & images

--- 10-LogisticReg/main.py
+++ 10-LogisticReg/main.py
@@ -1,16 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 # read kaggle facial expression recognition challenge dataset (fer2013.csv)
 # https://www.kaggle.com/c/challenges-in-representation-learning-facial-expression-recognition-challenge
 import numpy as np
+import pickle
 import matplotlib.pyplot as plt
+
+
 
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
 def get_data():
     # angry, disgust, fear, happy, sad, surprise, neutral
+    #               
     with open("fer2013.csv") as f:
-        content = f.readlines()
+            content = f.readlines()
 
     lines = np.array(content)
     num_of_instances = lines.size
@@ -77,10 +83,15 @@ class Model():
         b_grad = np.sum(pred-gt)/image.shape[0]
         self.b -= b_grad*self.lr
 
+
 def train(model):
+    auxTr=[]
+    auxTe=[]
+    auxEp=[]
+    #--------------------------------------
     x_train, y_train, x_test, y_test = get_data()
-    batch_size = 100 # Change if you want
-    epochs = 40000 # Change if you want
+    batch_size = 100# Change if you want
+    epochs = 10000 # Change if you want
     for i in range(epochs):
         loss = []
         for j in range(0,x_train.shape[0], batch_size):
@@ -92,21 +103,190 @@ def train(model):
         out = model.forward(x_test)                
         loss_test = model.compute_loss(out, y_test)
         print('Epoch {:6d}: {:.5f} | test: {:.5f}'.format(i, np.array(loss).mean(), loss_test))
-	# plot()
+        auxEp.append(i)
+        auxTr.append(np.array(loss).mean())
+        auxTe.append(loss_test)
+        plot(auxEp,auxTr,auxTe,epochs)
+    
 
-def plot(): # Add arguments
+
+        
+
+def plot(ep,tr,te,maxEpoch): # Add arguments
     # CODE HERE
     # Save a pdf figure with train and test losses
-    pass
-
+    aux=maxEpoch-1
+    if ep[len(ep)-1]==aux:
+        fig=plt.figure()
+        
+    plt.plot(ep,tr,'m',ep,te,'b')
+    plt.title('Train & Test loss vs Epoch')
+    plt.grid(True)
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend(('Train','Test'))
+    if ep[len(ep)-1]==aux:
+        fig.savefig("myGraph.pdf", bbox_inches='tight')
+        plt.show()
+        
+    
+    
+    
 def test(model):
-    # _, _, x_test, y_test = get_data()
+     _, _, x_test, y_test = get_data()
+     out = model.forward(x_test)
+     Threshs=np.linspace(-2,2,50)
+     prec=[]
+     reca=[]
+     fMeasure=[]
+     ACAs=[]
+     for th in Threshs:
+         
+         outBin=out>th                
+         numInstance=len(y_test)
+         confMat=np.zeros([2,2])
+         ins=0
+         tp=0
+         tn=0
+         fp=0
+         fn=0
+         for idx in range(numInstance):
+             
+            if outBin[idx]==y_test[idx]:
+                 
+                 if y_test[idx]==1:
+                     confMat[0,0]=confMat[0,0]+1
+                     ins=ins+1
+                     tp=tp+1
+
+                 else:
+                     confMat[1,1]=confMat[1,1]+1  
+                     ins=ins+1
+                     tn=tn+1
+
+            else:                            
+                  if y_test[idx]==1:
+                      confMat[0,1]=confMat[0,1]+1
+                      ins=ins+1
+                      fn=fn+1
+                 
+                  else:
+                      confMat[1,0]=confMat[1,0]+1
+                      ins=ins+1
+                      fp=fp+1
+                                     
+         confMat=confMat/ins            
+         ACA=confMat[1,1]+confMat[0,0]
+         ACAs.append(ACA)
+         auxP=(tp)/((tp)+(fp))
+         auxR=(tp)/((tp)+(fn))
+         FM=(2*auxP*auxR)/(auxP+auxR)
+         prec.append(auxP)
+         reca.append(auxR)
+         fMeasure.append(FM)
+         
+     maxF=np.max(fMeasure)
+     jdx=0
+     while maxF!=fMeasure[jdx]:
+         jdx=jdx+1
+         
+     PR=plt.figure()
+     plt.plot(reca,prec)
+     plt.grid(True)
+     plt.title('Precision-Recall curve - F1='+str(maxF)+' in threshold='+str(Threshs[jdx])+', ACA='+str(ACAs[jdx]))
+     plt.xlabel('Recall')
+     plt.ylabel('Precision')
+     PR.savefig('Precision-Recall.PNG')
+     plt.show()
+     
+     with open('model.pickle', 'wb') as f:
+        pickle.dump(model, f)              
     # YOU CODE HERE
     # Show some qualitative results and the total accuracy for the whole test set
-    pass
+    
 
 if __name__ == '__main__':
-    model = Model()
-    train(model)
-    test(model)
+    import argparse    
+    try:
+        print('Dataset Found')
+    except FileNotFoundError:    
+        
+        import os
+        print('Downloading Dataset...')
+        os.system('wget http://bcv001.uniandes.edu.co/fer2013.zip')
+        os.system('unzip fer2013.zip')
+        os.system('rm -rf fer2013.zip')
+        print('Dataset succesfully downloaded')
+   ##-----------------------------------------## 
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--test',action="store_true")
+    parser.add_argument('--demo',action="store_true")
+    args = parser.parse_args()
+    
+    if args.test:  
+        try:
+            with open('model.pickle','rb') as f:
+                model=pickle.load(f)
+                
+            print('Using Pre-Trained Model')    
+        except FileNotFoundError:
+            model=Model()
+            print('Pre-Trained Model not Found, training new Model...')
+            train(model)
+        
+        test(model) 
+    elif args.demo:
+        import skimage.io as io
+        import skimage.color as col
+        import skimage.transform as tm
+        #import os
+        try:
+            with open('model.pickle','rb') as f:
+                model=pickle.load(f)   
+                
+            print('Using Pre-Trained Model')
+        except FileNotFoundError:
+            model=Model()
+            print('Pre-Trained Model not Found, training new Model...')
+            train(model)
+            
+        #if (os.path.exists("./images"))==False:
+        #    print('Images folder not found')
+            
+        from os import listdir
+        from os.path import isfile, join
+        Files = [f for f in listdir('images') if isfile(join('images', f))]
+        images=np.zeros((len(Files),48,48))
+        for idx in range(len(Files)):
+            img=io.imread('images/'+Files[idx])
+            img=col.rgb2gray(img)
+            img=tm.resize(img, (48, 48), anti_aliasing=True)
+            images[idx,:,:]=img
+            OGimg=io.imread('images/'+Files[idx])
+            print('Processing image '+str(idx+1)+' out of '+str(len(Files)))
+        results=model.forward(images)
+            
+        indices=np.random.permutation(len(Files))
+                
+        for idx in range(1,7):
+            i=indices[idx]
+            if results[i]>0:
+                aux='happy'
+            else:
+                aux='not happy'
+                
+            plt.subplot(3,2,idx)
+            plt.imshow(images[i,:,:],cmap='gray')
+            plt.axis('off')
+            plt.title('classified as '+aux)
+        plt.show()                  
+        
+        
+    else:
+        model = Model()
+        train(model)
+        test(model)
+    
+    
 
