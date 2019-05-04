519a73713166f026cd3659d1a1a95a28e2b6c035
Santiago <s.martinez1@uniandes.edu.co>
2019-05-04 09:38:58 -0500

    codes

--- 13-FCN/16s_Train.py
+++ 13-FCN/16s_Train.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Fully Convolutional Network - Semantic Segmentation
+# # Fully Convolutional Network - FCN16s
 # 
-# ![image.png](imgs/1.png)
+# ![image.png](imgs/2.png)
+
+# ![image.png](imgs/3.png)
 
 # In[1]:
 
@@ -26,22 +28,20 @@ configurations = {
     # https://github.com/shelhamer/fcn.berkeleyvision.org
     1: dict(
         max_iteration=100000,
-        lr=1.0e-10,
+        lr=1.0e-12,
         momentum=0.99,
         weight_decay=0.0005,
-        interval_validate=8000000000000000000000,
+        interval_validate=4000
     )
 }
 
 resume = ''
 
 
-os.system('mkdir productImage')
 # In[2]:
 
 
 cfg = configurations[1]
-print(cfg)
 
 
 # In[3]:
@@ -69,20 +69,19 @@ def get_log_dir(model_name, config_id, cfg):
 # In[4]:
 
 
-out = get_log_dir('fcn32s', 1, cfg)
+out = get_log_dir('fcn16s', 1, cfg)
 print(out)
 
 
 # In[5]:
 
 
-gpu = 1
+gpu = 0
 os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu)
 cuda = torch.cuda.is_available()
-print('Cuda: {}'.format(cuda))
 
 
-# ## PascalVOC Dataset - Downloaded on _`root`_ variable
+# ## PascalVOC Dataset - Previously downloaded on _`root`_
 
 # In[6]:
 
@@ -188,8 +187,7 @@ class VOCClassSegBase(data.Dataset):
 
 class SBDClassSeg(VOCClassSegBase):
 
-    # XXX: It must be renamed to benchmark.tar to be extracted.
-    url = 'http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz'  # NOQA
+    url = 'http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/semantic_contours/benchmark.tgz'
 
     def __init__(self, root, split='train', transform=False):
         self.root = root
@@ -268,7 +266,7 @@ val_loader = torch.utils.data.DataLoader(
 # In[11]:
 
 
-#get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('matplotlib', 'inline')
 import matplotlib.pyplot as plt
 for data, target in train_loader: break
 print(data.shape)
@@ -277,8 +275,7 @@ data.min()
 data_show, label_show = train_loader.dataset.untransform(data[0].cpu().clone(), target[0].cpu().clone())
 
 plt.imshow(data_show)
-plt.savefig('productImage/data')
-#plt.show()
+plt.show()
 
 def imshow_label(label_show):
     import matplotlib
@@ -293,13 +290,12 @@ def imshow_label(label_show):
     plt.imshow(label_show, cmap=cmap, norm=norm)
     cbar = plt.colorbar(ticks=bounds)
     cbar.ax.set_yticklabels(train_loader.dataset.class_names)
-    plt.savefig('productImage/label')
-    #plt.show()    
+    plt.show()    
     
 imshow_label(label_show)
 
 
-# ## FCN - Model
+# ## FCN16s - Model
 
 # In[12]:
 
@@ -307,7 +303,212 @@ imshow_label(label_show)
 import numpy as np
 import torch.nn as nn
 
+class FCN16s(nn.Module):
+
+    def __init__(self, n_class=21):
+        super(FCN16s, self).__init__()
+        # conv1
+        self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100)
+        self.relu1_1 = nn.ReLU(inplace=True)
+        self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/2
+
+        # conv2
+        self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
+        self.relu2_1 = nn.ReLU(inplace=True)
+        self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
+        self.relu2_2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/4
+
+        # conv3
+        self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
+        self.relu3_1 = nn.ReLU(inplace=True)
+        self.conv3_2 = nn.Conv2d(256, 256, 3, padding=1)
+        self.relu3_2 = nn.ReLU(inplace=True)
+        self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
+        self.relu3_3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8
+
+        # conv4
+        self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
+        self.relu4_1 = nn.ReLU(inplace=True)
+        self.conv4_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.relu4_2 = nn.ReLU(inplace=True)
+        self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.relu4_3 = nn.ReLU(inplace=True)
+        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/16
+
+        # conv5
+        self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
+        self.relu5_1 = nn.ReLU(inplace=True)
+        self.conv5_2 = nn.Conv2d(512, 512, 3, padding=1)
+        self.relu5_2 = nn.ReLU(inplace=True)
+        self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
+        self.relu5_3 = nn.ReLU(inplace=True)
+        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/32
+
+        # fc6
+        self.fc6 = nn.Conv2d(512, 4096, 7)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.drop6 = nn.Dropout2d()
+
+        # fc7
+        self.fc7 = nn.Conv2d(4096, 4096, 1)
+        self.relu7 = nn.ReLU(inplace=True)
+        self.drop7 = nn.Dropout2d()
+
+        self.score_fr = nn.Conv2d(4096, n_class, 1)
+        self.score_pool4 = nn.Conv2d(512, n_class, 1)
+
+        self.upscore2 = nn.ConvTranspose2d(
+            n_class, n_class, 4, stride=2, bias=False)
+        self.upscore16 = nn.ConvTranspose2d(
+            n_class, n_class, 32, stride=16, bias=False)
+
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.zero_()
+                if m.bias is not None:
+                    m.bias.data.zero_()
+            if isinstance(m, nn.ConvTranspose2d):
+                assert m.kernel_size[0] == m.kernel_size[1]
+                initial_weight = get_upsampling_weight(
+                    m.in_channels, m.out_channels, m.kernel_size[0])
+                m.weight.data.copy_(initial_weight)
+
+    def forward(self, x, debug=False):
+        h = x
+        if debug: print(h.data.shape)
+        h = self.relu1_1(self.conv1_1(h))
+        if debug: print(h.data.shape)
+        h = self.relu1_2(self.conv1_2(h))
+        if debug: print(h.data.shape)
+        h = self.pool1(h)
+        if debug: print(h.data.shape)
+
+        h = self.relu2_1(self.conv2_1(h))
+        if debug: print(h.data.shape)
+        h = self.relu2_2(self.conv2_2(h))
+        if debug: print(h.data.shape)
+        h = self.pool2(h)
+        if debug: print(h.data.shape)
+
+        h = self.relu3_1(self.conv3_1(h))
+        if debug: print(h.data.shape)
+        h = self.relu3_2(self.conv3_2(h))
+        if debug: print(h.data.shape)
+        h = self.relu3_3(self.conv3_3(h))
+        if debug: print(h.data.shape)
+        h = self.pool3(h)
+        if debug: print(h.data.shape)
+
+        h = self.relu4_1(self.conv4_1(h))
+        if debug: print(h.data.shape)
+        h = self.relu4_2(self.conv4_2(h))
+        if debug: print(h.data.shape)
+        h = self.relu4_3(self.conv4_3(h))
+        if debug: print(h.data.shape)
+        h = self.pool4(h)
+        if debug: print('pool4: {}'.format(h.data.shape))
+        pool4 = h  # 1/16 #<------------------------------------
+
+        h = self.relu5_1(self.conv5_1(h))
+        if debug: print(h.data.shape)
+        h = self.relu5_2(self.conv5_2(h))
+        if debug: print(h.data.shape)
+        h = self.relu5_3(self.conv5_3(h))
+        if debug: print(h.data.shape)
+        h = self.pool5(h)
+        if debug: print(h.data.shape)
+
+        h = self.relu6(self.fc6(h))
+        if debug: print(h.data.shape)
+        h = self.drop6(h)
+        if debug: print(h.data.shape)
+
+        h = self.relu7(self.fc7(h))
+        if debug: print(h.data.shape)
+        h = self.drop7(h)
+        if debug: print(h.data.shape)
+
+        h = self.score_fr(h)
+        if debug: print(h.data.shape)
+        h = self.upscore2(h)
+        if debug: print('upscore2: {}'.format(h.data.shape))
+        upscore2 = h  # 1/16
+
+        h = self.score_pool4(pool4)
+        if debug: print('score_pool4: {}'.format(h.data.shape))
+        h = h[:, :, 5:5 + upscore2.size()[2], 5:5 + upscore2.size()[3]]
+        if debug: print('score_pool4c: {}'.format(h.data.shape))
+        score_pool4c = h  # 1/16
+
+        h = upscore2 + score_pool4c
+        if debug: print('upscore2+score_pool4c: {}'.format(h.data.shape))
+
+        h = self.upscore16(h)
+        if debug: print('upscore16: {}'.format(h.data.shape))
+        h = h[:, :, 27:27 + x.size()[2], 27:27 + x.size()[3]].contiguous()
+        if debug: print('upscore16 rearranged: {}'.format(h.data.shape))
+
+        return h
+
+    def copy_params_from_fcn32s(self, fcn32s):
+        for name, l1 in fcn32s.named_children():
+            try:
+                l2 = getattr(self, name)
+                l2.weight  # skip ReLU / Dropout
+            except Exception:
+                continue
+            assert l1.weight.size() == l2.weight.size()
+            assert l1.bias.size() == l2.bias.size()
+            l2.weight.data.copy_(l1.weight.data)
+            l2.bias.data.copy_(l1.bias.data)
+
+
+# In[13]:
+
+
+# https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
+def get_upsampling_weight(in_channels, out_channels, kernel_size):
+    """Make a 2D bilinear kernel suitable for upsampling"""
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:kernel_size, :kernel_size]
+    filt = (1 - abs(og[0] - center) / factor) *            (1 - abs(og[1] - center) / factor)
+    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
+                      dtype=np.float64)
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return torch.from_numpy(weight).float()
+
+
+# ## From FCN32s weights
+
+# In[14]:
+
+
+import numpy as np
+import torch.nn as nn
+import fcn
+
 class FCN32s(nn.Module):
+    
+    pretrained_model = osp.expanduser(os.getcwd()+'/data/pretrained_models/fcn32s_from_caffe.pth')
+
+    @classmethod
+    def download(cls):
+        return fcn.data.cached_download(
+            url='http://drive.google.com/uc?id=0B9P1L--7Wd2vM2oya3k0Zlgtekk',
+            path=cls.pretrained_model,
+            md5='8acf386d722dc3484625964cbe2aba49',
+        )    
 
     def __init__(self, n_class=21):
         super(FCN32s, self).__init__()
@@ -366,223 +567,27 @@ class FCN32s(nn.Module):
         self.upscore = nn.ConvTranspose2d(n_class, n_class, 64, stride=32,
                                           bias=False)
 
-        self._initialize_weights()
-
-    def _initialize_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                m.weight.data.zero_()
-                if m.bias is not None:
-                    m.bias.data.zero_()
-            if isinstance(m, nn.ConvTranspose2d):
-                assert m.kernel_size[0] == m.kernel_size[1]
-                initial_weight = get_upsampling_weight(
-                    m.in_channels, m.out_channels, m.kernel_size[0])
-                m.weight.data.copy_(initial_weight)
-
-    def forward(self, x, debug = False):
-        h = x
-        if debug: print(h.data.shape)
-        h = self.relu1_1(self.conv1_1(h))
-        if debug: print(h.data.shape)
-        h = self.relu1_2(self.conv1_2(h))
-        if debug: print(h.data.shape)
-        h = self.pool1(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu2_1(self.conv2_1(h))
-        if debug: print(h.data.shape)
-        h = self.relu2_2(self.conv2_2(h))
-        if debug: print(h.data.shape)
-        h = self.pool2(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu3_1(self.conv3_1(h))
-        if debug: print(h.data.shape)
-        h = self.relu3_2(self.conv3_2(h))
-        if debug: print(h.data.shape)
-        h = self.relu3_3(self.conv3_3(h))
-        if debug: print(h.data.shape)
-        h = self.pool3(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu4_1(self.conv4_1(h))
-        if debug: print(h.data.shape)
-        h = self.relu4_2(self.conv4_2(h))
-        if debug: print(h.data.shape)
-        h = self.relu4_3(self.conv4_3(h))
-        if debug: print(h.data.shape)
-        h = self.pool4(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu5_1(self.conv5_1(h))
-        if debug: print(h.data.shape)
-        h = self.relu5_2(self.conv5_2(h))
-        if debug: print(h.data.shape)
-        h = self.relu5_3(self.conv5_3(h))
-        if debug: print(h.data.shape)
-        h = self.pool5(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu6(self.fc6(h))
-        if debug: print(h.data.shape)
-        h = self.drop6(h)
-        if debug: print(h.data.shape)
-
-        h = self.relu7(self.fc7(h))
-        if debug: print(h.data.shape)
-        h = self.drop7(h)
-        if debug: print(h.data.shape)
-
-        h = self.score_fr(h)
-        if debug: print(h.data.shape)
-
-        h = self.upscore(h)
-        if debug: print(h.data.shape)
-        h = h[:, :, 19:19 + x.size()[2], 19:19 + x.size()[3]].contiguous()
-        if debug: print(h.data.shape)
-            
-        return h
-
-    def copy_params_from_vgg16(self, vgg16):
-        features = [
-            self.conv1_1, self.relu1_1,
-            self.conv1_2, self.relu1_2,
-            self.pool1,
-            self.conv2_1, self.relu2_1,
-            self.conv2_2, self.relu2_2,
-            self.pool2,
-            self.conv3_1, self.relu3_1,
-            self.conv3_2, self.relu3_2,
-            self.conv3_3, self.relu3_3,
-            self.pool3,
-            self.conv4_1, self.relu4_1,
-            self.conv4_2, self.relu4_2,
-            self.conv4_3, self.relu4_3,
-            self.pool4,
-            self.conv5_1, self.relu5_1,
-            self.conv5_2, self.relu5_2,
-            self.conv5_3, self.relu5_3,
-            self.pool5,
-        ]
-        for l1, l2 in zip(vgg16.features, features):
-            if isinstance(l1, nn.Conv2d) and isinstance(l2, nn.Conv2d):
-                assert l1.weight.size() == l2.weight.size()
-                assert l1.bias.size() == l2.bias.size()
-                l2.weight.data = l1.weight.data
-                l2.bias.data = l1.bias.data
-        for i, name in zip([0, 3], ['fc6', 'fc7']):
-            l1 = vgg16.classifier[i]
-            l2 = getattr(self, name)
-            l2.weight.data = l1.weight.data.view(l2.weight.size())
-            l2.bias.data = l1.bias.data.view(l2.bias.size())
-
-
-# In[13]:
-
-
-# https://github.com/shelhamer/fcn.berkeleyvision.org/blob/master/surgery.py
-def get_upsampling_weight(in_channels, out_channels, kernel_size):
-    """Make a 2D bilinear kernel suitable for upsampling"""
-    factor = (kernel_size + 1) // 2
-    if kernel_size % 2 == 1:
-        center = factor - 1
-    else:
-        center = factor - 0.5
-    og = np.ogrid[:kernel_size, :kernel_size]
-    filt = (1 - abs(og[0] - center) / factor) *            (1 - abs(og[1] - center) / factor)
-    weight = np.zeros((in_channels, out_channels, kernel_size, kernel_size),
-                      dtype=np.float64)
-    weight[range(in_channels), range(out_channels), :, :] = filt
-    return torch.from_numpy(weight).float()
-
-
-# ## From VGG16 weights
-
-# In[14]:
-
-
-import torchvision
-import fcn #PIP INSTALL
-
-def VGG16(pretrained=True, folder='data/pretrained_models'):
-    model = torchvision.models.vgg16(pretrained=False)
-    if not pretrained:
-        return model
-    model_file = _get_vgg16_pretrained_model(folder)
-    
-    state_dict = torch.load(model_file)
-    model.load_state_dict(state_dict)
-    return model
-
-
-def _get_vgg16_pretrained_model(folder):
-    path_model = osp.join(os.getcwd(), folder, 'vgg16_from_caffe.pth')
-    return fcn.data.cached_download(
-                url='http://drive.google.com/uc?id=0B9P1L--7Wd2vLTJZMXpIRkVVRFk',
-                path=path_model,
-                md5='aa75b158f4181e7f6230029eb96c1b13',
-            )
-            
-
 
 # In[15]:
 
 
-model = FCN32s(n_class=21)
-if cuda:
-    model.to('cuda')
+model = FCN16s(n_class=21)
+if cuda: model.to('cuda')
 
 
-# In[16]:
+# In[ ]:
 
 
-iter_loader=iter(train_loader)
-data, target = next(iter_loader)
-if cuda:
-    data = data.to('cuda')
-with torch.no_grad():
-    output = model(data)
+model
 
 
-# In[17]:
+# In[ ]:
 
 
-print('output: ', output.data.shape)
-print('input: ', data.shape)
+cfg['fcn32s_pretrained_model'] = FCN32s.download() # Original FCN32 pretrained model
 
 
-# In[18]:
-
-
-data, target = next(iter_loader)
-if cuda:
-    data = data.to('cuda')
-with torch.no_grad():
-    output = model(data, debug=True)
-
-
-# In[19]:
-
-
-data, target = next(iter_loader)
-if cuda:
-    data = data.to('cuda')
-with torch.no_grad():
-    output = model(data, debug=True)
-
-
-# In[20]:
-
-
-data, target = next(iter_loader)
-if cuda:
-    data = data.to('cuda')
-with torch.no_grad():
-    output = model(data, debug=True)
-
-
-# In[21]:
+# In[ ]:
 
 
 if resume:
@@ -590,19 +595,20 @@ if resume:
     checkpoint = torch.load(resume)
     model.load_state_dict(checkpoint['model_state_dict'])
 else:
-    vgg16 = VGG16(pretrained=True) # It takes a while
-    model.copy_params_from_vgg16(vgg16)
+    fcn32s = FCN32s()
+    fcn32s.load_state_dict(torch.load(cfg['fcn32s_pretrained_model']))
+    model.copy_params_from_fcn32s(fcn32s)
 
 
 # ## Optimizer
 
-# In[22]:
+# In[ ]:
 
 
-for idx, m in enumerate(model.modules()): print(str(idx)+' - '+ str(m)+'\n----')
+print(cfg['fcn32s_pretrained_model'])
 
 
-# In[23]:
+# In[ ]:
 
 
 def get_parameters(model, bias=False):
@@ -630,7 +636,7 @@ def get_parameters(model, bias=False):
             raise ValueError('Unexpected module: %s' % str(m))
 
 
-# In[24]:
+# In[ ]:
 
 
 optim = torch.optim.SGD([
@@ -643,7 +649,7 @@ optim = torch.optim.SGD([
         )
 
 
-# In[25]:
+# In[ ]:
 
 
 if resume:
@@ -652,10 +658,10 @@ if resume:
 
 # ## Loss
 
-# In[26]:
+# In[ ]:
 
 
-#get_ipython().run_line_magic('matplotlib', 'inline')
+get_ipython().run_line_magic('matplotlib', 'inline')
 import datetime
 from distutils.version import LooseVersion
 import math
@@ -697,7 +703,7 @@ def cross_entropy2d(input, target, weight=None, size_average=True):
     return loss
 
 
-# In[27]:
+# In[ ]:
 
 
 class Trainer(object):
@@ -795,8 +801,7 @@ class Trainer(object):
         #scipy.misc.imsave(out_file, img_)
         imageio.imwrite(out_file, img_)
         plt.imshow(imageio.imread(out_file))
-        plt.savefig('productImage/validate')
-        #plt.show()
+        plt.show()
 
         val_loss /= len(self.val_loader)
 
@@ -887,7 +892,7 @@ class Trainer(object):
                 break
 
 
-# In[28]:
+# In[ ]:
 
 
 trainer = Trainer(
@@ -902,24 +907,7 @@ trainer = Trainer(
         )
 
 
-# In[29]:
-
-
-model.train()
-data, target = next(iter_loader)
-if cuda:
-    model.to('cuda')
-    data, target = data.to('cuda'), target.to('cuda')
-
-
-# In[30]:
-
-
-print(cfg.get('interval_validate', len(train_loader))) #Validate every 4000 iterations
-print(out)
-
-
-# In[32]:
+# In[ ]:
 
 
 start_epoch = 0
